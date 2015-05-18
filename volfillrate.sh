65a564f78c76d2ec5e2c7336849532d1f138b30a
Danny Sheehan <danny@ftmon.org>
2015-05-18 10:17:48 +1000

    use posix version of df and fix divide by zero bug

--- volfillrate.sh
+++ volfillrate.sh
@@ -13,23 +13,28 @@
 
 FS_MOUNT=$1
 SAMPLE_INTERVAL=60
-SAMPLE=100
 
 takesample()
 {
-  BEFORE_USAGE=`df -k $FS_MOUNT | tail -1 | awk '{print $3}'`
+  BEFORE_USAGE=`df -k -P $FS_MOUNT | tail -1 | awk '{print $3}'`
   sleep $SAMPLE_INTERVAL
-  AFTER_USAGE=`df -k $FS_MOUNT | tail -1 | awk '{print $3}'`
+  AFTER_USAGE=`df -k -P $FS_MOUNT | tail -1 | awk '{print $3}'`
 
-  AVAILABLE=`df -k $FS_MOUNT | tail -1 | awk '{print $4}'`
-  FILLRATE=`echo "scale=2;($AFTER_USAGE - $BEFORE_USAGE) / 1024 / $SAMPLE_INTERVAL" | bc -l`
-  FULLIN=`echo "scale=2;$AVAILABLE / ($AFTER_USAGE - $BEFORE_USAGE) * $SAMPLE_INTERVAL / 60 / 60 / 24" | bc -l`
-  MEMFREE=$(( $AVAILABLE / 1024 ))
-  echo "$FILLRATE Mbytes/Sec, $MEMFREE Mbytes free,  Full in  $FULLIN days"
+  if [ $AFTER_USAGE -eq $BEFORE_USAGE ]
+  then
+    echo -n "."
+  else
+    echo
+    AVAILABLE=`df -k -P $FS_MOUNT | tail -1 | awk '{print $4}'`
+    FILLRATE=`echo "scale=2;($AFTER_USAGE - $BEFORE_USAGE) / 1024 / $SAMPLE_INTERVAL" | bc -l`
+    FULLIN=`echo "scale=2;$AVAILABLE / ($AFTER_USAGE - $BEFORE_USAGE) * $SAMPLE_INTERVAL / 60 / 60 / 24" | bc -l`
+    MEMFREE=$(( $AVAILABLE / 1024 ))
+    echo "$FILLRATE Mbytes/Sec, $MEMFREE Mbytes free,  Full in  $FULLIN days"
+  fi
 }
 
 
-for ((i=1;i<=SAMPLE;i++))
+while true
 do
   takesample
 done

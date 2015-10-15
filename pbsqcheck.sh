9065f791afdd79c978f6e9203e22cfc447a50eba
Danny Sheehan <danny@ftmon.org>
2015-10-15 12:06:10 +1000

    Backup of some silly cludge scripts i have used.

--- pbsqcheck.sh
+++ pbsqcheck.sh
@@ -9,11 +9,17 @@
 #  Usage: 
 #
 # --------------------------------------------------------------------------
+#./pbsqcheck.sh: line 66: 160 / 
+#16:ncpus=2:NodeType=medium:mem=10GB:mpiprocs=2: :
+
+echo "   WAIT_DAYS JOB_ID NODES NODETYPE MEM (MEMSET) CPU_HRS / NCPUS = WALLTIME" 
 QTIMESTR=""
 qstat -f `qselect -sQ` | \
-  egrep "(qtime|Resource_List.select|Resource_List.mem|Job Id:|Resource_List.cpu_hrs|Resource_List.ncpus)" | \
+  egrep "(qtime|Resource_List.select|Resource_List.mem|Job Id:|Resource_List.cpu_hrs|Resource_List.ncpus|Submit_arguments)" | \
      while read l
 do
+  ADMIN_MSG=`qstat -s $JOB_ID 2> /dev/null | tail -1 | awk -F\: '{print $2}' | grep -v "too few free"`
+  ANYNODE=""
 
   if [  "${l}" != "${l/Job Id:/}" ]
   then
@@ -22,6 +28,16 @@ do
   elif [ "${l}" != "${l/Resource_List.mem/}" ]
   then
     MEM=`echo $l | awk '{print $3}'`
+    if [ "${MEM}" != "${MEM/mb/}" ]
+    then
+      MEM=${MEM/mb/}
+      if [ $MEM -gt 1000 ]
+      then
+        MEM=`echo "scale=0;$MEM / 1000" | bc -l`
+      else
+        MEM=1
+      fi
+    fi
     MEM=${MEM/gb/}
 
   elif [ "${l}" != "${l/Resource_List.cpu_hrs/}" ]
@@ -38,19 +54,11 @@ do
 
   elif [ "${l}" != "${l/Resource_List.select/}" ]
   then
-    SELECT=`echo $l | awk '{print $3}' | sed -e "s/:sche.*$/:/"`
-    NODETYPE=`echo $SELECT | sed -e "s/^.*:NodeType=\([^:]*\):.*$/\1/"` 
-    if [ "$NODETYPE" = "$SELECT" ]
-    then
-       NODETYPE="none"
-    fi
-    NODES=`echo $SELECT | sed -e "s/^\([0-9]\):.*$/\1/"`
-    MEMSET=`echo $SELECT | sed -e "s/^.*:mem=\([0-9GgmbB]*\):.*$/\1/"` 
-    if [ "$MEMSET" = "$SELECT" ]
-    then
-       MEMSET="none"
-       continue
-    fi
+    SELECT=`echo $l | awk '{print $3}' | sed -e "s/:sched.*//"`
+    NODES=`echo $SELECT | tr ':' '\n' | head -1`
+    NODETYPE=`echo $l | tr ':' '\n' | grep "NodeType=" | cut -d\= -f2`
+    MEMSET=`echo $l | tr ':' '\n' | grep "mem=" | cut -d\= -f2`
+    NCPUSET=`echo $l | tr ':' '\n' | grep "ncpus=" | cut -d\= -f2`
 
     if [ -n "$QTIMESTR" ]
     then
@@ -59,15 +67,76 @@ do
       WAIT_TIME=$(( ($NOW - $QTIME) / 60 / 60 / 24 ))
     fi
 
-    ADMIN_MSG=`qstat -s $JOB_ID | tail -1 | awk -F\: '{print $2}' | grep -v "too few free"`
-   
+
+  elif [  "${l}" != "${l/Submit_arguments/}" ]
+  then
+    ANYNODE=`echo $l | grep -i "NodeType=any"`
+
+    WALLTIME=$(( $CPU_HRS / $NCPUS ))
+
     if [ "${MEM}" != "${MEM/[0-9]*/}" ]
     then
       MEM=$((  $MEM / $NODES )) 
     fi
-    echo "$WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $NCPUS $CPU_HRS"
-    if [ -n "${ADMIN_MSG}" ]; then echo "    '$ADMIN_MSG'";fi 
-        echo "    select=$SELECT"
+
     QTIMESTR=""
+    if [ -z "$MEM" ]
+    then
+      echo ">> $JOB_ID did not specify mem:"
+      echo "   $WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $CPU_HRS / $NCPUS = $WALLTIME" 
+      #if [ -n "${ADMIN_MSG}" ]; then echo "    '$ADMIN_MSG'";fi 
+      #    echo "    select=$SELECT"
+    fi
+
+    echo "   $WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $CPU_HRS / $NCPUS = $WALLTIME" 
+    if [ -n "${ADMIN_MSG}" ]; then echo "    '$ADMIN_MSG'";fi 
+    continue
+
+    if [ -z "$NODETYPE" ] && [ $WALLTIME -lt 24 ]
+    then
+      echo ">> $JOB_ID specified nodetype any with $WALLTIME < 24 hrs"
+      echo "   $WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $CPU_HRS / $NCPUS = $WALLTIME" 
+    fi
+
+    if [ "$NODETYPE" = "xl" ] && [ $MEM -lt 71 ]
+    then
+      echo ">> $JOB_ID specified nodetype xl with $MEM < 71g"
+      echo "   $WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $CPU_HRS / $NCPUS = $WALLTIME" 
+    fi
+
+    if [ "$NODETYPE" = "xl" ] && [ $MEM -gt 1000 ]
+    then
+      echo ">> $JOB_ID specified nodetype xl with $MEM > 1000g"
+      echo "   $WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $CPU_HRS / $NCPUS = $WALLTIME" 
+    fi
+
+    if [ "$NODETYPE" = "medium" ] && [ $MEM -gt 22 ]
+    then
+      echo ">> $JOB_ID specified nodetype medim with $MEM > 22g"
+      echo "   $WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $CPU_HRS / $NCPUS = $WALLTIME" 
+    fi
+
+    if [ "$NODETYPE" = "large" ] && [ $MEM -lt 23 ]
+    then
+      echo ">> $JOB_ID specified nodetype large with $MEM < 23g"
+      echo "   $WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $CPU_HRS / $NCPUS = $WALLTIME" 
+    fi
+
+    if [ "$NODETYPE" = "large" ] && [ $MEM -gt 70 ]
+    then
+      echo ">> $JOB_ID specified nodetype large with $MEM > 70g"
+      echo "   $WAIT_TIME $JOB_ID $NODES $NODETYPE $MEM ($MEMSET) $CPU_HRS / $NCPUS = $WALLTIME" 
+    fi
+
   fi
 done
+
+echo "Free xl"
+echo "-------"
+pbsnodes2 -1 -e jobs -e NodeType -e comment | grep "NodeType:xl" | grep -v jobs
+
+echo
+echo "Free large"
+echo "----------"
+pbsnodes2 -1 -e jobs -e NodeType -e comment | grep "NodeType:large" | grep -v jobs | grep -v comment
+
